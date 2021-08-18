ff5c764e9f816804af6bd168e5435b9f7618b91f
Mr.Cristofer <88551911+Cristofer6010@users.noreply.github.com>
2021-08-18 08:45:06 -0700

    Create trafic_drive_age.py

---
+++ trafic_drive_age.py
@@ -0,0 +1,11 @@
+print ("Enter your age :")
+b = int(input())
+
+if b==18:
+    print ("We can't decide come in our company we test you physicaly")
+elif b<18:
+    print ("You can't drive")
+elif b<=100:
+    print ("You can drive")
+elif b>100:
+    print ("Invalid age")
